55986b74414487ccc4fb14769fe45dac7fb389da
[therk] <[mju5906105378@mju.ac.th]>
2022-04-10 15:28:06 +0700

    master

---
+++ pages/hero.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>My Heroes</h1>
+    <ul v-for="(hero, index) in heroes" :key="index">
+      <li @click="selectHero(hero.name)">{{ hero.number }} {{ hero.name }}</li>
+    </ul>
+
+    My hero is <u>{{ hero }}</u>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      heroes: [
+        { number: "11", name: "Dr Nice" },
+        { number: "12", name: "necromancer" },
+        { number: "13", name: "Bombbasto" },
+        { number: "14", name: "Celeritas" },
+        { number: "15", name: "Magneta" },
+        { number: "16", name: "RubberMan" },
+        { number: "17", name: "Dynama" },
+        { number: "18", name: "Dr IQ" },
+        { number: "19", name: "Magma" },
+        { number: "20", name: "Tornado" },
+      ],
+      hero: null,
+    };
+  },
+  methods: {
+    selectHero(heroName) {
+      this.hero = heroName;
+    },
+  },
+};
+</script>
---
+++ pages/posts.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <b-table :fields="fields" :items="items"></b-table>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      fields: [
+          {
+            key: 'Id',
+            sortable: true
+          },
+          {
+            key: 'userId',
+            sortable: true
+          },
+          {
+            key: 'title', 
+            sortable: true,
+          },
+          {
+            key: 'body', 
+            sortable: true,
+          }
+        ],
+    };
+  },
+  async asyncData({ $axios }) {
+    const items = await $axios.$get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    return { items };
+  },
+
+  methods: {},
+};
+</script>
